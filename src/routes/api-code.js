cf4b79edd0a0fd57d757b6946581931c15cab1fc
Ed Bowler <ed.bowler@equalexperts.com>
2026-01-28 17:38:19 +0000

    DWT-967: actually add files

---
+++ src/routes/api-code.js
@@ -0,0 +1,62 @@
+// import Boom from '@hapi/boom'
+import { paths } from '../config/paths.js'
+import { mergeAndValidate } from '../domain/organisation.js'
+import {
+  findOrganisationById,
+  orgCollection
+} from '../repositories/organisation.js'
+import { updateWithOptimisticLock } from '../repositories/index.js'
+
+export const apiCodes = [
+  {
+    method: 'GET',
+    path: paths.listApiCodes,
+    options: { auth: 'api-key-auth' },
+    handler: async (request, h) => {
+      const { apiCodes } = await findOrganisationById(
+        request.db,
+        request.params.organisationId
+      )
+      return h.response({ message: 'success', apiCodes })
+    }
+  },
+  {
+    method: 'PUT',
+    path: paths.saveApiCode,
+    options: { auth: 'api-key-auth' },
+    handler: async (request, h) => {
+      try {
+        const organisation = await updateWithOptimisticLock(
+          request.db.collection(orgCollection),
+          { organisationId: request.params.organisationId },
+          (dbOrg) => {
+            const organisationId = request.params.organisationId
+            const apiCodes = { ...dbOrg.apiCodes }
+            apiCodes[request.params.apiCode] = {
+              name: request.payload?.apiCode?.name,
+              apiCode: request.params.apiCode
+            }
+            if (request.payload?.apiCode?.disabled) {
+              apiCodes[request.params.apiCode].disabled = true
+            }
+            return mergeAndValidate(
+              dbOrg,
+              { organisationId, apiCodes },
+              organisationId,
+              null
+            )
+          }
+        )
+        return h.response({
+          message: 'success',
+          apiCode: organisation.apiCodes[request.params.apiCode]
+        })
+      } catch (e) {
+        return h.response({
+          message: 'error',
+          errors: e.isJoi ? e.details : [`${e}`]
+        })
+      }
+    }
+  }
+]
