6d1f4965eb2933bfe9074be4dfdff0d966c821e6
Philip J Smith <philip.j.smith@contractsquared.com>
2026-02-09 15:17:48 +0000

    removed dotenv

--- src/config.js
+++ src/config.js
@@ -9,7 +9,6 @@ convict.addFormats(convictFormatWithValidator)
 const isProduction = process.env.NODE_ENV === 'production'
 const isTest = process.env.NODE_ENV === 'test'
 
-
 export const config = convict({
   auth: {
     clients: {
