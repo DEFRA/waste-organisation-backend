558384dbe19ffb8cbe5458e802f3f237d50085ff
Ed Bowler <ed.bowler@equalexperts.com>
2026-02-13 12:10:40 +0000

    DWT-1426 upload spreadsheet (#15)
    * DWT-1426: upload spreadsheet
    
    * DWT-1426: fix typo
    
    * DWT-1426: fix imports
    
    * DWT-1426: change default config
    
    * DWT-1426: upload spreadsheet - maps to json structure. no errors
    
    * DWT-1426: fix component concentrations
    
    * DWT-1426: started error tracking
    
    * DWT-1426: upload spreadsheet WIP
    
    * DWT-1426: upload spreadsheet WIP
    
    * DWT-1426: upload spreadsheet WIP
    
    * DWT-1426: upload spreadsheet WIP
    
    * DWT-1426: debugging writing spreadsheet
    
    * DWT-1426: Writing errors into spreadsheet mostly works
    
    * DWT-1426: linter changes
    
    * DWT-1426: remove console log
    
    * DWT-1426: increase timeout for running on ci
    
    * DWT-1426: make code less readable because sonar says so
    
    * DWT-1426: sonar changes
    
    * DWT-1426: merge cos sonar said so
    
    * DWT-1426: name constants to try and appease the sonar cargo cult
    
    * DWT-1426: sacrifice a goat to sonar
    
    * DWT-1426: rename unused
    
    * DWT-1426: ignore sonar rule
    
    * DWT-1426: removed unused

--- src/services/spreadsheetImport.js
+++ src/services/spreadsheetImport.js
@@ -1,15 +1,381 @@
 import Excel from 'exceljs'
 import { createLogger } from '../common/helpers/logging/logger.js'
 
-/* v8 ignore start */
 const logger = createLogger()
 
-export const parseExcelFile = async (buffer) => {
-  const workbook = new Excel.Workbook()
-  await workbook.xlsx.load(buffer)
-  workbook.eachSheet((worksheet, _sheetId) => {
-    logger.info(`worksheet ${worksheet.name}`)
-  })
-  return workbook
+const cellError = (colNumber, rowNumber, message) => ({
+  coords: [colNumber, rowNumber],
+  message
+})
+
+const cellValueText = (() => {
+  const plainText = (x) => x?.text ?? x
+  return (v) => {
+    if (Array.isArray(v)) {
+      return v.reduce((acc, x) => acc + plainText(x.richText ?? x), '')
+    } else {
+      return plainText(v)
+    }
+  }
+})()
+
+const stripFormatting = (cell) => {
+  cell.style = {
+    border: {
+      left: { style: 'thin' },
+      right: { style: 'thin' },
+      top: { style: 'thin' },
+      bottom: { style: 'thin' }
+    }
+  }
+  return cell
 }
+
+const emptyErrorCell = () => ({ richText: [] })
+
+const collectCellErrors = (
+  errors,
+  updateFn,
+  r,
+  [colNumber, rowNumber],
+  cell
+) => {
+  try {
+    updateFn(r, [colNumber, rowNumber], cellValueText(cell.value))
+  } catch (e) {
+    errors.push(cellError(colNumber, rowNumber, e.message))
+  }
+}
+
+const worksheetToArray = ({ worksheet, keyCol, updateFn, minRow, maxCol }) => {
+  const elements = []
+  const errors = []
+  worksheet.eachRow((row, rowNumber) => {
+    if (rowNumber > minRow && row.getCell(keyCol).value) {
+      row.getCell(1).value = emptyErrorCell()
+      const r = {}
+      row.eachCell((cell, colNumber) => {
+        stripFormatting(cell)
+        if (colNumber < maxCol) {
+          collectCellErrors(errors, updateFn, r, [colNumber, rowNumber], cell)
+        }
+      })
+      r['--rowNumber'] = rowNumber
+      elements.push(r)
+    }
+  })
+  return { elements, errors }
+}
+
+export const updateErrors = (() => {
+  const font = {
+    bold: true,
+    size: 12,
+    color: { argb: 'FFD4351C' },
+    name: 'Calibri'
+  }
+  const fillStyle = {
+    type: 'pattern',
+    pattern: 'solid',
+    fgColor: { argb: 'FFFFCCCC' },
+    bgColor: { argb: 'FFFFD9D9' }
+  }
+  const updateCell = (worksheet, coords, message) => {
+    const [colNumber, rowNumber] = coords
+    const row = worksheet.getRow(rowNumber)
+    const cell = row.getCell(colNumber)
+    const errorCell = row.getCell(1)
+    if (errorCell) {
+      const v = errorCell?.value?.richText ? errorCell?.value : { richText: [] }
+      v.richText.push({
+        font,
+        text: v.richText.length > 0 ? '\n' : '' + message
+      })
+      errorCell.value = v
+    }
+    if (cell?.value) {
+      cell.value = { richText: [{ font, text: cell.value }] }
+      cell.style.fill = fillStyle
+    }
+  }
+  return (workbook, cellsAndMessages) => {
+    for (const worksheetName of Object.keys(cellsAndMessages)) {
+      const worksheet = workbook.getWorksheet(worksheetName)
+      for (const { coords, message } of cellsAndMessages[worksheetName]) {
+        updateCell(worksheet, coords, message)
+      }
+    }
+    return workbook
+  }
+})()
+
+export const parseExcelFile = async (buffer) => {
+  logger.info('Starting parsing spreadsheet')
+  const workbook = new Excel.Workbook()
+  await workbook.xlsx.load(buffer, {
+    ignoreNodes: [
+      // 'autoFilter',
+      // 'cols',
+      'conditionalFormatting' // breaks generated excel file
+      // 'dataValidations',
+      // 'dimension',
+      // 'drawing',
+      // 'extLst',
+      // 'headerFooter',
+      // 'hyperlinks',
+      // 'mergeCells',
+      // 'pageMargins',
+      // 'pageSetup',
+      // 'picture',
+      // 'printOptions',
+      // 'rowBreaks',
+      // 'sheetData', // ignores actual data
+      // 'sheetFormatPr',
+      // 'sheetPr',
+      // 'sheetProtection',
+      // 'sheetViews',
+      // 'tableParts'
+    ]
+  })
+  const movements = worksheetToArray({
+    worksheet: workbook.getWorksheet('7. Waste movement level'),
+    keyCol: 3,
+    minRow: 8,
+    maxCol: 32,
+    updateFn: movementColName
+  })
+  const items = worksheetToArray({
+    worksheet: workbook.getWorksheet('8. Waste item level'),
+    keyCol: 2,
+    minRow: 8,
+    maxCol: 25,
+    updateFn: itemColName
+  })
+  const joined = joinWasteItems(movements.elements, items.elements)
+  if (
+    movements.errors.length > 0 ||
+    items.errors.length > 0 ||
+    joined.errors.length > 0
+  ) {
+    const errors = {
+      '7. Waste movement level': movements.errors.concat(
+        joined.errors.movements
+      ),
+      '8. Waste item level': items.errors.concat(joined.errors.items)
+    }
+    updateErrors(workbook, errors)
+    return {
+      errors,
+      workbook
+    }
+  } else {
+    return joined
+  }
+}
+
+const joinWasteItems = (movements, items) => {
+  const is = groupBy((x) => x['yourUniqueReference'], items)
+  const errors = { movements: [], items: [] }
+  const movementRefCol = 3
+  const itemRefCol = 2
+  for (let i = 0; i < movements.length; i++) {
+    const r = movements[i]['yourUniqueReference']
+    if (is[r] && is[r].length > 0) {
+      movements[i].wasteItems = is[r].map((x) => {
+        delete x['--rowNumber']
+        return x
+      })
+      delete movements[i]['--rowNumber']
+      delete is[r]
+    } else {
+      errors.movements.push(
+        cellError(
+          movementRefCol,
+          movements[i]['--rowNumber'],
+          'No waste items for unique reference'
+        )
+      )
+    }
+  }
+  if (Object.keys(is).length > 0) {
+    for (const m of Object.values(is).flatMap((x) => x)) {
+      errors.items.push(
+        cellError(
+          itemRefCol,
+          m['--rowNumber'],
+          'No waste movements for unique reference'
+        )
+      )
+    }
+  }
+  return { movements, errors }
+}
+
+const groupBy = (func, list) => {
+  return list.reduce((acc, x) => {
+    if (acc[func(x)] == null) {
+      acc[func(x)] = []
+    }
+    acc[func(x)].push(x)
+    return acc
+  }, {})
+}
+
+const updateData = (cols) => {
+  const updateIn = (data, path, v, func) => {
+    path.reduce((acc, x, i) => {
+      // prettier-ignore
+      if (i === path.length - 1) {
+        const value = func ? func(acc[x], v) : v
+        acc[x] = value
+      } else if (acc[x] == null) { // nosonar
+        acc[x] = {}
+      }
+      return acc[x]
+    }, data)
+    return data
+  }
+
+  return (r, [colNum, _rowNum], value) => {
+    const cs = cols[colNum]
+    if (typeof cs[cs.length - 1] === 'function') {
+      updateIn(r, cs.slice(0, -1), value, cs[cs.length - 1])
+    } else {
+      updateIn(r, cs, value)
+    }
+    return r
+  }
+}
+
+const parseComponentCodes = (existing, data) => {
+  const result = existing ?? []
+  try {
+    result.concat(
+      data.split(/;/).map((y) => {
+        // eslint-disable-next-line no-unused-vars
+        const [_, code, concentration] = y
+          .match(/([^=]*)=(.*)/) // nosonar
+          .map((x) => x.trim())
+        return { code, concentration }
+      })
+    )
+    return result
+  } catch (e) {
+    throw new Error('Cannot parse component codes')
+  }
+}
+
+const parseComponentNames = (existing, data) => {
+  const result = existing ?? []
+  try {
+    const parsed = data.split(/;/).flatMap((y) => {
+      // eslint-disable-next-line no-unused-vars
+      const [_, name, concentration] = y
+        .match(/([^=]*)=(.*)/) // nosonar
+        .map((x) => x.trim())
+      return { code: name, concentration }
+    })
+    return result.concat(parsed)
+  } catch (e) {
+    throw new Error('Cannot parse component names')
+  }
+}
+
+const mergeDate = (existing, data) => {
+  if (!(data instanceof Date)) {
+    throw new Error('Cannot parse date')
+  }
+  if (existing == null) {
+    return data
+  } else {
+    const date = new Date(existing.getTime())
+    date.setFullYear(data.getFullYear())
+    date.setMonth(data.getMonth())
+    date.setDate(data.getDate())
+    return date
+  }
+}
+
+const mergeTime = (existing, data) => {
+  if (!(data instanceof Date)) {
+    throw new Error('Cannot parse time')
+  }
+  if (existing == null) {
+    return data
+  } else {
+    const date = new Date(existing.getTime())
+    date.setHours(data.getHours())
+    date.setMinutes(data.getMinutes())
+    date.setSeconds(data.getSeconds())
+    return date
+  }
+}
+
+const parseDisposalCodes = (_existing, data) => {
+  const [code, metric, amount, est] = data.split(/=/).map((x) => x.trim())
+  if (est) {
+    const isEstimate = est.toLowerCase()
+    return { code, weight: { metric, amount, isEstimate } }
+  } else {
+    throw new Error('Cannot parse disposal codes.')
+  }
+}
+
+const movementColName = updateData([
+  [],
+  [],
+  [],
+  ['yourUniqueReference'],
+  ['receiver', 'siteName'],
+  ['receipt', 'address', 'fullAddress'],
+  ['receipt', 'address', 'postcode'],
+  ['receiver', 'authorisationNumber'],
+  ['receiver', 'regulatoryPositionStatement'],
+  ['receiver', 'emailAddress'],
+  ['receiver', 'phoneNumber'],
+  ['dateTimeReceived', mergeDate],
+  ['dateTimeReceived', mergeTime],
+  ['hazardousWasteConsignmentCode'],
+  ['reasonForNoConsignmentCode'],
+  ['specialHandlingRequirements'],
+  ['carrier', 'registrationNumber'],
+  ['carrier', 'reasonForNoRegistrationNumber'],
+  ['carrier', 'organisationName'],
+  ['carrier', 'address', 'fullAddress'],
+  ['carrier', 'address', 'postcode'],
+  ['carrier', 'emailAddress'],
+  ['carrier', 'phoneNumber'],
+  ['carrier', 'meansOfTransport'],
+  ['carrier', 'vehicleRegistration'],
+  ['brokerOrDealer', 'organisationName'],
+  ['brokerOrDealer', 'address', 'fullAddress'],
+  ['brokerOrDealer', 'address', 'postcode'],
+  ['brokerOrDealer', 'emailAddress'],
+  ['brokerOrDealer', 'phoneNumber'],
+  ['brokerOrDealer', 'registrationNumber']
+])
+
+const itemColName = updateData([
+  [],
+  [],
+  ['yourUniqueReference'],
+  ['ewcCodes'],
+  ['wasteDescription'],
+  ['physicalForm'],
+  ['numberOfContainers'],
+  ['typeOfContainers'],
+  ['weight', 'metric'],
+  ['weight', 'amount'],
+  ['weight', 'isEstimate'],
+  ['containsPops'],
+  ['pops', 'components', parseComponentCodes],
+  ['pops', 'sourceOfComponents'],
+  ['containsHazardous'],
+  ['hazardous', 'hazCodes'],
+  ['hazardous', 'components', parseComponentNames],
+  ['hazardous', 'sourceOfComponents'],
+  ['disposalOrRecoveryCodes', parseDisposalCodes]
+])
+
+/* v8 ignore start */
 /* v8 ignore stop */
---
+++ src/services/spreadsheetImport.test.js
@@ -0,0 +1,241 @@
+import fs from 'node:fs/promises'
+import { parseExcelFile } from './spreadsheetImport.js'
+
+describe('excel proccessor', () => {
+  test('should parse buffer', { timeout: 100000 }, async () => {
+    const buffer = await fs.readFile(
+      './test-resources/example-spreadsheet.xlsx'
+    )
+    const { movements, errors } = await parseExcelFile(buffer)
+    expect(movements[0].dateTimeReceived).toEqual(
+      new Date('2026-01-14T11:05:00.000Z')
+    )
+    expect(movements).toEqual([
+      {
+        carrier: {
+          organisationName: 'Qualitech Environmental Services Ltd',
+          registrationNumber: 'CBDU171976'
+        },
+        dateTimeReceived: new Date('2026-01-14T11:05:00.000Z'),
+        hazardousWasteConsignmentCode: 'KAWASA/19963',
+        receipt: {
+          address: {
+            fullAddress: 'Ernesettle Lane, Plumouth',
+            postcode: 'PL5 2SA'
+          }
+        },
+        receiver: {
+          siteName: 'Kawasaki Precision Machinery UK Ltd'
+        },
+        yourUniqueReference: 'KAWASA/19963',
+        wasteItems: [
+          {
+            containsHazardous: true,
+            containsPops: false,
+            disposalOrRecoveryCodes: {
+              code: 'D09',
+              weight: {
+                amount: 'kg',
+                isEstimate: 'estimate',
+                metric: '10,000'
+              }
+            },
+            ewcCodes: '06 01 10',
+            hazardous: {
+              components: [
+                {
+                  code: 'Hydrochloric Acid',
+                  concentration: '<=37%'
+                },
+                {
+                  code: 'Water',
+                  concentration: 'Balance'
+                }
+              ],
+              hazCodes: 'HP5, HP8',
+              sourceOfComponents: 'PROVIDED_WITH_WASTE'
+            },
+            numberOfContainers: 1,
+            physicalForm: 'Liquid',
+            typeOfContainers: '[TAN] Tanker/Tank',
+            wasteDescription: 'Hydrochloric Pickling Acid',
+            weight: {
+              amount: 10000,
+              isEstimate: true,
+              metric: 'Kilograms'
+            },
+            yourUniqueReference: 'KAWASA/19963'
+          }
+        ]
+      }
+    ])
+    expect(errors).toEqual({
+      items: [
+        {
+          coords: [2, 10],
+          message: 'No waste movements for unique reference'
+        },
+        {
+          coords: [2, 11],
+          message: 'No waste movements for unique reference'
+        }
+      ],
+      movements: []
+    })
+  })
+
+  test('should write errors buffer', { timeout: 10000 }, async () => {
+    const buffer = await fs.readFile(
+      './test-resources/example-spreadsheet-2.xlsx'
+    )
+    const { errors, workbook } = await parseExcelFile(buffer)
+    expect(errors).toEqual({
+      '7. Waste movement level': [
+        {
+          coords: [12, 9],
+          message: 'Cannot parse time'
+        }
+      ],
+      '8. Waste item level': [
+        {
+          coords: [18, 9],
+          message: 'Cannot parse disposal codes.'
+        },
+        {
+          coords: [12, 10],
+          message: 'Cannot parse component codes'
+        },
+        {
+          coords: [16, 10],
+          message: 'Cannot parse component names'
+        },
+        {
+          coords: [2, 10],
+          message: 'No waste movements for unique reference'
+        },
+        {
+          coords: [2, 11],
+          message: 'No waste movements for unique reference'
+        }
+      ]
+    })
+    await workbook.xlsx.writeFile(
+      './test-resources/output-spreadsheet-2-with-errors.xlsx'
+    )
+  })
+})
+
+/*
+const example = {
+  apiCode: 'ba6eb330-4f7f-11eb-a2fb-67c34e9ac07cg',
+  dateTimeReceived:
+    'UTC - 2025-09-15T12:12:28Z, BST - 2025-09-15T13:12:28+01:00',
+  hazardousWasteConsignmentCode:
+    'Company name: CJTILE Ltd → Code prefix: CJTILE/\nUnique ID: A0001\nFull code: CJTILE/A0001\n',
+  reasonForNoConsignmentCode: 'Carrier did not provide documentation',
+  yourUniqueReference: 'wTBrdgAA020',
+  otherReferencesForMovement: [
+    {
+      label: 'PO Number',
+      reference: 'PO-12345'
+    },
+    {
+      label: 'Waste Ticket',
+      reference: 'WT-67890'
+    },
+    {
+      label: 'Haulier Note',
+      reference: 'HN-11111'
+    }
+  ],
+  specialHandlingRequirements:
+    'The waste must be fully inspected by the waste handler according to the Hazardous waste consignment and or EWC codes provided.',
+  wasteItems: [
+    {
+      ewcCodes:
+        '200108 biodegradable kitchen and canteen waste, 150109 textile packaging',
+      wasteDescription:
+        'Basic mixed construction and demolition waste, this includes recyclable house bricks, gypsum plaster and slates.',
+      physicalForm: 'Sludge',
+      numberOfContainers: '2, 3',
+      typeOfContainers: 'BAG, PAL, SKI',
+      weight: {
+        metric: 'Tonnes',
+        amount: 150,
+        isEstimate: true
+      },
+      containsPops: true,
+      pops: {
+        sourceOfComponents: 'PROVIDED_WITH_WASTE',
+        components: [
+          {
+            code: 'PFHXS, HBB, END, PCDD_PCDF',
+            concentration: '12.5, 9,21234, 500'
+          }
+        ]
+      },
+      containsHazardous: true,
+      hazardous: {
+        sourceOfComponents: 'PROVIDED_WITH_WASTE',
+        hazCodes:
+          'HP 5 - Wastes from petroleum refining, Natural Gas Purification and pyrolitic treatment of coal, HP 10 - Wastes from Thermal Processes</p>',
+        components: [
+          {
+            name: 'lead, mercury',
+            concentration: '50, 12.5, 25.5'
+          }
+        ]
+      },
+      disposalOrRecoveryCodes: [
+        {
+          code: '"R1"\n',
+          weight: {
+            metric: 'Tonnes',
+            amount: 150,
+            isEstimate: true
+          }
+        }
+      ]
+    }
+  ],
+  carrier: {
+    registrationNumber:
+      'England - CBDL6, CBDU45, CBDU84916, Wales - CBDL1, CBDU33, CBDU84916, Scotland - WCR/522048, Northern Ireland - ROC UT 36, ROC LT 5432',
+    reasonForNoRegistrationNumber:
+      'Waste carrier did not provide a carrier registration number.',
+    organisationName: 'Waste Carriers Lite Ltd',
+    address: {
+      fullAddress: '26a Oil Drum Lane, London, UK',
+      postcode: 'W12 7ZL'
+    },
+    emailAddress: 'info@wastecarrierselite.co.uk',
+    phoneNumber: '020 4756 XXXX',
+    vehicleRegistration: 'RNT 493',
+    meansOfTransport: 'Rail'
+  },
+  brokerOrDealer: {
+    organisationName: 'Waste Desposal Ltd',
+    address: {
+      fullAddress: '26a Oil Drum Lane, London, UK',
+      postcode: 'W12 7ZL'
+    },
+    emailAddress: 'info@wastecarrierselite.co.uk',
+    phoneNumber: '020 4756 XXXX',
+    registrationNumber:
+      'CBDL6, CBDU45, CBDU84916, Wales - CBDL1, CBDU33, Scotland - WCR/R/522048, Northern Ireland - ROC UT 36, ROC LT 5432'
+  },
+  receiver: {
+    siteName: 'string',
+    emailAddress: 'info@wastecarrierselite.co.uk',
+    phoneNumber: '020 4756 XXXX',
+    authorisationNumber: 'HP3456XX',
+    regulatoryPositionStatements: [343, 456, 789]
+  },
+  receipt: {
+    address: {
+      fullAddress: '26a Oil Drum Lane, London, UK',
+      postcode: 'W12 7ZL'
+    }
+  }
+}
+*/
