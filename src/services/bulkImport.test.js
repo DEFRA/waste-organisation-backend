d63096445085e4e04c3fd87626d1b66ed5246849
Ed Bowler <ed.bowler@equalexperts.com>
2026-02-19 10:31:19 +0000

    DWT-1426: uncomment test

--- src/services/bulkImport.test.js
+++ src/services/bulkImport.test.js
@@ -16,7 +16,7 @@ describe('bulk import data', () => {
     expect(res.errors).toBe(undefined)
   })
 
-  test.skip('should convert error messages from data import', { timeout: 50000 }, async () => {
+  test('should convert error messages from data import', { timeout: 50000 }, async () => {
     const conf = {
       endpoint: 'http://localhost:3002',
       url: '/bulk/{{bulkUploadId}}/movements/receive',
@@ -33,34 +33,15 @@ describe('bulk import data', () => {
         message: 'ewc codes must be a valid EWC code from the official list'
       },
       {
-        coords: [9, 7],
-        errorValue: '[TAN] Tanker/Tank',
-        message: 'type of containers must be a valid container type'
-      },
-      {
         coords: [9, 16],
         errorValue: [
           {
-            code: 'Hydrochloric Acid',
-            concentration: '<=37%'
+            concentration: '<=37%',
+            name: 'Hydrochloric Acid'
           },
           {
-            code: 'Water',
-            concentration: 'Balance'
-          }
-        ],
-        message: 'name is required'
-      },
-      {
-        coords: [9, 16],
-        errorValue: [
-          {
-            code: 'Hydrochloric Acid',
-            concentration: '<=37%'
-          },
-          {
-            code: 'Water',
-            concentration: 'Balance'
+            concentration: 'Balance',
+            name: 'Water'
           }
         ],
         message: 'concentration must be a number'
@@ -69,60 +50,15 @@ describe('bulk import data', () => {
         coords: [9, 16],
         errorValue: [
           {
-            code: 'Hydrochloric Acid',
-            concentration: '<=37%'
+            concentration: '<=37%',
+            name: 'Hydrochloric Acid'
           },
           {
-            code: 'Water',
-            concentration: 'Balance'
-          }
-        ],
-        message: 'name is required'
-      },
-      {
-        coords: [9, 16],
-        errorValue: [
-          {
-            code: 'Hydrochloric Acid',
-            concentration: '<=37%'
-          },
-          {
-            code: 'Water',
-            concentration: 'Balance'
+            concentration: 'Balance',
+            name: 'Water'
           }
         ],
         message: 'concentration must be a number'
-      },
-      {
-        coords: [9, 16],
-        errorValue: [
-          {
-            code: 'Hydrochloric Acid',
-            concentration: '<=37%'
-          },
-          {
-            code: 'Water',
-            concentration: 'Balance'
-          }
-        ],
-        message: 'code is not allowed'
-      },
-      {
-        coords: [9, 18],
-        errorValue: {
-          code: 'D09',
-          weight: {
-            amount: '10,000',
-            isEstimate: true,
-            metric: 'kg'
-          }
-        },
-        message: 'disposal or recovery codes must be an array'
-      },
-      {
-        coords: [9, 2],
-        errorValue: 'KAWASA/19963',
-        message: 'your unique reference is not allowed'
       },
       {
         coords: [9, 23],
@@ -142,48 +78,13 @@ describe('bulk import data', () => {
       },
       {
         errorType: 'UnexpectedError',
-        key: '0.wasteItems.0.typeOfContainers',
-        message: '"[0].wasteItems[0].typeOfContainers" must be a valid container type'
-      },
-      {
-        errorType: 'UnexpectedError',
-        key: '0.wasteItems.0.hazardous.components.0.name',
-        message: '"[0].wasteItems[0].hazardous.components[0].name" is required'
-      },
-      {
-        errorType: 'UnexpectedError',
         key: '0.wasteItems.0.hazardous.components.0.concentration',
         message: '"[0].wasteItems[0].hazardous.components[0].concentration" must be a number'
       },
       {
         errorType: 'UnexpectedError',
-        key: '0.wasteItems.0.hazardous.components.0.code',
-        message: '"[0].wasteItems[0].hazardous.components[0].code" is not allowed'
-      },
-      {
-        errorType: 'UnexpectedError',
-        key: '0.wasteItems.0.hazardous.components.1.name',
-        message: '"[0].wasteItems[0].hazardous.components[1].name" is required'
-      },
-      {
-        errorType: 'UnexpectedError',
         key: '0.wasteItems.0.hazardous.components.1.concentration',
         message: '"[0].wasteItems[0].hazardous.components[1].concentration" must be a number'
-      },
-      {
-        errorType: 'UnexpectedError',
-        key: '0.wasteItems.0.hazardous.components.1.code',
-        message: '"[0].wasteItems[0].hazardous.components[1].code" is not allowed'
-      },
-      {
-        errorType: 'UnexpectedError',
-        key: '0.wasteItems.0.disposalOrRecoveryCodes',
-        message: '"[0].wasteItems[0].disposalOrRecoveryCodes" must be an array'
-      },
-      {
-        errorType: 'UnexpectedError',
-        key: '0.wasteItems.0.yourUniqueReference',
-        message: '"[0].wasteItems[0].yourUniqueReference" is not allowed'
       },
       {
         errorType: 'UnexpectedError',
