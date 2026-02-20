e429f3b8e8946c37bbe3aeb9d1c7d7283db1afa9
Philip J Smith <philip.j.smith@contractsquared.com>
2026-02-20 15:07:01 +0000

    created notify tests

--- src/services/notify/index.test.js
+++ src/services/notify/index.test.js
@@ -1,33 +1,33 @@
 import { describe, expect, it, vi } from 'vitest'
 
-// import { config } from '../../config'
-// const apiKey = config.get('notify.govNotifyKey')
+import { config } from '../../config'
+const apiKey = config.get('notify.govNotifyKey')
 
-// const email = 'someone@example.com'
+const email = 'someone@example.com'
 
-// describe.skipIf(!apiKey)('Intergration tests when apiCode is set', () => {
-//   const file = Buffer.from('{"test": "123"}', 'utf8')
+describe.skipIf(!apiKey)('Intergration tests when apiCode is set', () => {
+  const file = Buffer.from('{"test": "123"}', 'utf8')
 
-//   it('should send success email', async () => {
-//     const emailResonse = await sendEmail.sendSuccess({ email, file })
-//     expect(emailResonse.status).toBe(201)
-//   })
+  it('should send success email', async () => {
+    const emailResonse = await sendEmail.sendSuccess({ email, file })
+    expect(emailResonse.status).toBe(201)
+  })
 
-//   it('should send data validation failed email', async () => {
-//     const emailResonse = await sendEmail.sendValidationFailed({ email, file })
-//     expect(emailResonse.status).toBe(201)
-//   })
+  it('should send data validation failed email', async () => {
+    const emailResonse = await sendEmail.sendValidationFailed({ email, file })
+    expect(emailResonse.status).toBe(201)
+  })
 
-//   it('should send upload failed email', async () => {
-//     const emailResonse = await sendEmail.sendFailed({ email, file })
-//     expect(emailResonse.status).toBe(201)
-//   })
+  it('should send upload failed email', async () => {
+    const emailResonse = await sendEmail.sendFailed({ email, file })
+    expect(emailResonse.status).toBe(201)
+  })
 
-//   it('should send email with an attachment', async () => {
-//     const emailResonse = await sendEmail.sendSuccess({ email, file })
-//     expect(emailResonse.status).toBe(201)
-//   })
-// })
+  it('should send email with an attachment', async () => {
+    const emailResonse = await sendEmail.sendSuccess({ email, file })
+    expect(emailResonse.status).toBe(201)
+  })
+})
 
 describe('Notify', () => {
   const prepareUploadMock = vi.fn()
