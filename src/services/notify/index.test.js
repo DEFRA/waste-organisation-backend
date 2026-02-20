aa73f12c2fe0735b36384acd9dfb6be5d556532f
Phillip Smith <46485944+philjzmith@users.noreply.github.com>
2026-02-20 15:36:57 +0000

    created notify tests (#22)
    * created notify tests
    
    * created notify tests
    
    * readded import
    
    * moved import
    
    * split intergraqtion
    
    * removed unused variable

--- src/services/notify/index.test.js
+++ src/services/notify/index.test.js
@@ -1,30 +1,64 @@
-import { expect, it } from 'vitest'
-import { sendEmail } from '.'
-import { config } from '../../config'
-const apiKey = config.get('notify.govNotifyKey')
+import { beforeAll, describe, expect, it, vi } from 'vitest'
 
-const email = 'someone@example.com'
+describe('Notify', () => {
+  const prepareUploadMock = vi.fn()
+  const sendEmailMock = vi.fn()
+  const loggerErrorMock = vi.fn()
+  const loggerInfoMock = vi.fn()
+  const email = 'foo@example.com'
 
-describe.skipIf(!apiKey)('Intergration tests when apiCode is set', () => {
-  const file = Buffer.from('{"test": "123"}', 'utf8')
+  const successfulSubmission = '2ffe3792-f097-421d-b3e2-9de5af81609f'
 
-  it('should send success email', async () => {
-    const emailResonse = await sendEmail.sendSuccess({ email, file })
-    expect(emailResonse.status).toBe(201)
+  beforeAll(() => {
+    vi.doMock('notifications-node-client', () => ({
+      NotifyClient: vi.fn().mockImplementation(() => ({
+        prepareUpload: prepareUploadMock.mockReturnValue('link'),
+        sendEmail: sendEmailMock
+      }))
+    }))
+
+    vi.doMock('pino', () => ({
+      pino: vi.fn().mockImplementation(() => ({
+        info: loggerInfoMock,
+        error: loggerErrorMock
+      }))
+    }))
   })
 
-  it('should send data validation failed email', async () => {
-    const emailResonse = await sendEmail.sendValidationFailed({ email, file })
-    expect(emailResonse.status).toBe(201)
+  it('should return email response', async () => {
+    sendEmailMock.mockReturnValue('response')
+    const { sendEmail } = await import('./index.js')
+    const actualResponse = await sendEmail.sendSuccess({ email })
+    const personalisation = {
+      'first name': 'Joe Bloggs'
+    }
+
+    expect(sendEmailMock).toBeCalledWith(successfulSubmission, email, { personalisation })
+    expect(actualResponse).toBe(sendEmailMock())
+    expect(loggerInfoMock).toBeCalledWith('Email Response response')
   })
 
-  it('should send upload failed email', async () => {
-    const emailResonse = await sendEmail.sendFailed({ email, file })
-    expect(emailResonse.status).toBe(201)
+  it('should return email response with file link', async () => {
+    sendEmailMock.mockReturnValue('response')
+    const { sendEmail } = await import('./index.js')
+    const file = Buffer.from([{ foo: 'bar' }])
+    await sendEmail.sendSuccess({ email, file })
+    const personalisation = {
+      'first name': 'Joe Bloggs',
+      link_to_file: 'link'
+    }
+
+    expect(sendEmailMock).toBeCalledWith(successfulSubmission, email, { personalisation })
+    expect(prepareUploadMock).toBeCalledWith(file)
   })
 
-  it('should send email with an attachment', async () => {
-    const emailResonse = await sendEmail.sendSuccess({ email, file })
-    expect(emailResonse.status).toBe(201)
+  it('should handle exception correctly', async () => {
+    sendEmailMock.mockRejectedValue('Mock Error')
+
+    const { sendEmail } = await import('./index.js')
+
+    await sendEmail.sendSuccess({ email })
+
+    expect(loggerErrorMock).toBeCalled('Error sending emails: Mock Error')
   })
 })
