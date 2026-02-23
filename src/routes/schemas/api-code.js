5571dc85bd14a997f207d4ba2cfc2b1c18564a84
Philip Morris <philip@ip-192-168-4-68.eu-west-2.compute.internal>
2026-02-23 18:15:19 +0000

    fix export

--- src/routes/schemas/api-code.js
+++ src/routes/schemas/api-code.js
@@ -1,6 +1,8 @@
 import joi from 'joi'
 import { apiCodeSchema } from '../../domain/organisation.js'
 
+export { apiCodeSchema as apiCodeResponseSchema } from '../../domain/organisation.js'
+
 export const lookupOrgResponseSchema = joi.object({
   defraOrganisationId: joi.string().required()
 })
@@ -8,5 +10,3 @@ export const lookupOrgResponseSchema = joi.object({
 export const listApiCodesResponseSchema = joi.object({
   apiCodes: joi.array().items(apiCodeSchema).required()
 })
-
-export const apiCodeResponseSchema = apiCodeSchema
