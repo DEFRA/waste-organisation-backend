ccec7b0c8786eab346ab36129b2e496f1e40db9f
Ed Bowler <ed.bowler@equalexperts.com>
2026-02-25 16:42:27 +0000

    DWT-1719: add some more logging

--- src/services/bulkImport.js
+++ src/services/bulkImport.js
@@ -5,21 +5,23 @@ import { pathTo } from '../config/paths.js'
 
 const logger = createLogger()
 
-const apiCall = async (asyncFunc, { username, password }, payload) => {
+const apiCall = async (asyncFunc, { username, password }, payload, uploadId) => {
   try {
     const headers = { Authorization: 'Basic ' + Buffer.from(username + ':' + password).toString('base64'), 'content-type': 'application/json' }
     const r = { json: 'strict', headers }
     if (payload) {
       r.payload = payload
     }
+    logger.debug(`UploadId: ${uploadId} -- Sending to Bulk API: ${JSON.stringify(payload)}`)
     const response = await asyncFunc(r)
+    logger.debug(`UploadId: ${uploadId} -- Result from Bulk API (status): ${JSON.stringify(response.payload)}`)
     return response.payload
   } catch (e) {
-    logger.error(`ERROR calling bulk import api ${e}`)
+    logger.error(`UploadId: ${uploadId} -- ERROR calling bulk import api ${e}`)
     // prettier-ignore
     if (e.output.statusCode === 400) { // nosonar
+      logger.debug(`UploadId: ${uploadId} -- Validation errors processing spreadsheet ${e.data}`)
       const errors = e.data.payload.flatMap((v) => v.validation.errors)
-      logger.info(`Info Validation errors processing spreadsheet`)
       return { errors }
     } else {
       throw e
@@ -32,7 +34,7 @@ const urlFor = (bulkUploadId, conf) => {
     const u = conf.endpoint.replace(/\/$/, '')
     return u + pathTo(conf.url, { bulkUploadId })
   } catch (e) {
-    logger.error(`Error generating bulk endpoint url ${conf}`)
+    logger.error(`UploadId: ${bulkUploadId} -- Error generating bulk endpoint url ${conf}`)
     throw e
   }
 }
@@ -40,6 +42,6 @@ const urlFor = (bulkUploadId, conf) => {
 export const bulkImport = async (bulkUploadId, movements, conf) => {
   const c = conf ?? config.get('bulkUpload')
   const url = urlFor(bulkUploadId, c)
-  const response = await apiCall((r) => wreck.post(url, r), c.basicAuth, movements)
+  const response = await apiCall((r) => wreck.post(url, r), c.basicAuth, movements, bulkUploadId)
   return response
 }
