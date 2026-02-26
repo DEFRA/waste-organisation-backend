25d0f9ca7a20f93c1b0be5a7b974d6274bcd0d3a
Ed Bowler <ed.bowler@equalexperts.com>
2026-02-26 09:32:13 +0000

    DWT-1751 validation error mapping fixes (#40)
    * DWT-1751: fix order of coords
    
    * DWT-1751: include cell address with error message
    
    * DWT-1751: wrap long line

--- src/services/spreadsheetImport.js
+++ src/services/spreadsheetImport.js
@@ -82,8 +82,13 @@ const worksheetToArray = ({ worksheet, keyCol, updateFn, minRow, maxCol }) => {
 export const updateErrors = (() => {
   const font = { bold: true, size: 12, color: { argb: 'FFD4351C' }, name: 'Calibri' }
   const fillStyle = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'FFFFCCCC' }, bgColor: { argb: 'FFFFD9D9' } }
+  // prettier-ignore
+  const colNames = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                    'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK']
+  const coordsToCellName = (coords) => ` (${colNames[coords[1] - 1]}${coords[0]})`
+
   const updateCell = (worksheet, coords, message) => {
-    const [colNumber, rowNumber] = coords
+    const [rowNumber, colNumber] = coords
     const row = worksheet.getRow(rowNumber)
     const cell = row.getCell(colNumber)
     const errorCell = row.getCell(1)
@@ -91,7 +96,7 @@ export const updateErrors = (() => {
       const v = errorCell?.value?.richText ? errorCell?.value : emptyCell()
       v.richText.push({
         font,
-        text: v.richText.length > 0 ? '\n' : '' + message
+        text: v.richText.length > 0 ? '\n' : '' + message + coordsToCellName(coords)
       })
       errorCell.value = v
     }
@@ -117,11 +122,11 @@ const updateData = (cols) => {
       path.reduce((acc, x, i) => {
         // prettier-ignore
         if (i === path.length - 1) {
-        const value = func ? func(acc[x], v) : v
-        acc[x] = value
-      } else if (acc[x] == null) { // nosonar
-        acc[x] = {}
-      }
+          const value = func ? func(acc[x], v) : v
+          acc[x] = value
+        } else if (acc[x] == null) { // nosonar
+          acc[x] = {}
+        }
         return acc[x]
       }, data)
     }
