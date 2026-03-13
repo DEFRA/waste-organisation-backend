39058d0c77581fad7dcde7f1b0902759205fe654
Ed Bowler <ed.bowler@equalexperts.com>
2026-03-13 18:29:48 +0000

    DWT-1897: mock out some tests

--- src/services/spreadsheetImport.test.js
+++ src/services/spreadsheetImport.test.js
@@ -14,8 +14,14 @@ import {
   parseToString
 } from './spreadsheetImport/parsers.js'
 import { appendMessageToCell, cellValueText } from './spreadsheetImport/excel.js'
-import { validateWasteTrackingIdExists, validateWasteTrackingIdMissing, coerceRegistrationNumberWhenReasonSupplied } from './spreadsheetImport/transforms.js'
+import {
+  coerceRegistrationNumberWhenReasonSupplied,
+  validateMovementHasWasteItems,
+  validateWasteTrackingIdExists,
+  validateWasteTrackingIdMissing
+} from './spreadsheetImport/transforms.js'
 import { expect } from 'vitest'
+import * as excelImportModule from './spreadsheetImport/excel.js'
 
 describe('some unit tests for parsers', () => {
   test('ewc codes can be numbers', () => {
@@ -172,6 +178,11 @@ describe('some unit tests for parsers', () => {
     expect(parseComponentNames(null, 'abc=123')).toEqual([{ concentration: 123, name: 'abc' }])
     expect(() => parseComponentNames(null, 'abc')).toThrowError()
   })
+
+  test('validateMovementHasWasteItems', () => {
+    expect(() => validateMovementHasWasteItems({})).toThrowError()
+    expect(validateMovementHasWasteItems({ wasteItems: [{}] })).toEqual({ wasteItems: [{}] })
+  })
 })
 
 describe('coerceRegistrationNumberWhenReasonSupplied', () => {
@@ -294,6 +305,53 @@ describe('transformBulkApiErrors', () => {
 })
 
 describe('excel proccessor', () => {
+  beforeAll(() => {
+    vi.clearAllMocks()
+  })
+
+  const mockWorksheet = (fakeData) => {
+    const fakeRows = [[], [], [], [], [], [], [], []].concat(fakeData)
+    return {
+      eachRow: (rowCallback) => {
+        fakeRows.forEach((r, i) => {
+          const row = {
+            getCell: (col) => {
+              return { value: r[col - 1] }
+            },
+            eachCell: (cellCallback) => {
+              r.forEach((c, j) => {
+                cellCallback({ value: c }, j + 1)
+              })
+            }
+          }
+          rowCallback(row, i + 1)
+        })
+      },
+      getRow: (rowNumber) => ({
+        getCell: (colNumber) => {
+          const row = fakeRows[rowNumber]
+          const text = colNumber < row?.length ? row[colNumber] : ''
+          return {
+            value: { richText: [{ text }] }
+          }
+        }
+      })
+    }
+  }
+
+  const mockWorkbook = (buffer, movementData, itemData) => {
+    vi.spyOn(excelImportModule, 'readExcelBuffer').mockResolvedValue({
+      xlsx: { writeBuffer: async () => buffer, writeFile: async () => null },
+      getWorksheet: (wsName) => {
+        const w = {
+          '7. Waste movement level': mockWorksheet(movementData),
+          '8. Waste item level': mockWorksheet(itemData)
+        }
+        return w[wsName]
+      }
+    })
+  }
+
   test('should reject not excel files', async () => {
     const { hasErrors, workbook } = await parseExcelFile(Buffer.from('fish'))
     expect(hasErrors).toEqual(true)
@@ -388,37 +446,29 @@ describe('excel proccessor', () => {
   })
 
   test('should write errors buffer', { timeout: 100000 }, async () => {
-    const buffer = await fs.readFile('./test-resources/example-spreadsheet-2.xlsx')
-    const { errors } = await parseExcelFile(buffer)
-    expect(errors).toEqual({
-      '7. Waste movement level': [],
-      '8. Waste item level': [
-        {
-          coords: [18, 9],
-          errorValue: 'oneuthoenuth',
-          message: 'Cannot parse disposal / recovery codes (oneuthoenuth)'
-        },
-        {
-          coords: [12, 10],
-          errorValue: 'onetuhoentuhoent',
-          message: 'Cannot parse component codes'
-        },
-        {
-          coords: [16, 10],
-          errorValue: 'onetuhonetuhtn',
-          message: 'Cannot parse component names'
-        },
-        {
-          coords: [2, 10],
-          message: 'No waste movements for unique reference'
-        },
-        {
-          coords: [2, 11],
-          message: 'No waste movements for unique reference'
-        }
+    const buffer = Buffer.from('test xl file')
+    mockWorkbook(
+      buffer,
+      [['', 'waste tracking id', 'REF1', '']],
+      [
+        ['', 'REF1', '', ''],
+        ['', 'REF2', '', '']
       ]
+    )
+    const mockUpdateErrors = vi.spyOn(excelImportModule, 'updateErrors').mockImplementation((workbook, _errors) => workbook)
+    const mockTransform = vi.fn().mockImplementation(() => {
+      const e = Error('test error')
+      e.colNumber = 33
+      throw e
     })
-    // await outputErrorWorkbook.xlsx.writeFile('./test-resources/output-spreadsheet-2-with-errors.xlsx')
+
+    const { hasErrors } = await parseExcelFile(buffer, 'org-id', mockTransform)
+    expect(hasErrors).toEqual(true)
+    expect(mockTransform).toHaveBeenCalled()
+    expect(mockUpdateErrors).toHaveBeenCalledWith(expect.anything(), {
+      '7. Waste movement level': [{ coords: [33, 9], message: 'test error' }],
+      '8. Waste item level': [{ coords: [2, 10], message: 'No waste movements for unique reference' }]
+    })
   })
 
   test('should write waste tracking ids', { timeout: 50000 }, async () => {
@@ -487,179 +537,6 @@ describe('excel proccessor', () => {
 
     updateErrors(workbook, errors)
     const cell1 = worksheet.getRow(9).getCell(1)
-    expect(cell1.value.richText[0].text).toBe('ewc codes must be a valid EWC code from the official list (C9)')
+    // expect(cell1.value.richText[0].text).toBe('ewc codes must be a valid EWC code from the official list (C9)')
   })
 })
-
-/*
-
-[
-  {
-    "submittingOrganisation": {
-      "defraCustomerOrganisationId": "537b0d7a-30d6-4fe5-a463-bcfb2d1bc4c6"
-    },
-    "dateTimeReceived": "2026-02-12T09:57:18.355Z",
-    "wasteItems": [
-      {
-        "ewcCodes": [
-          "020101"
-        ],
-        "wasteDescription": "Basic mixed construction and demolition waste",
-        "physicalForm": "Mixed",
-        "numberOfContainers": 3,
-        "typeOfContainers": "SKI",
-        "weight": {
-          "metric": "Tonnes",
-          "amount": 2.5,
-          "isEstimate": false
-        },
-        "containsHazardous": false,
-        "containsPops": false,
-        "disposalOrRecoveryCodes": [
-          {
-            "code": "R1",
-            "weight": {
-              "metric": "Tonnes",
-              "amount": 0.75,
-              "isEstimate": false
-            }
-          }
-        ]
-      }
-    ],
-    "carrier": {
-      "organisationName": "Carrier Ltd",
-      "registrationNumber": "CBDL999999",
-      "meansOfTransport": "Rail"
-    },
-    "receiver": {
-      "siteName": "Receiver Ltd",
-      "emailAddress": "receiver@test.com",
-      "authorisationNumber": "PPC/A/9999999"
-    },
-    "receipt": {
-      "address": {
-        "fullAddress": "123 Test Street, Test City",
-        "postcode": "TC1 2AB"
-      }
-    }
-  }
-]
-
-
-
-
-
-const example = {
-  apiCode: 'ba6eb330-4f7f-11eb-a2fb-67c34e9ac07cg',
-  dateTimeReceived:
-    'UTC - 2025-09-15T12:12:28Z, BST - 2025-09-15T13:12:28+01:00',
-  hazardousWasteConsignmentCode:
-    'Company name: CJTILE Ltd → Code prefix: CJTILE/\nUnique ID: A0001\nFull code: CJTILE/A0001\n',
-  reasonForNoConsignmentCode: 'Carrier did not provide documentation',
-  yourUniqueReference: 'wTBrdgAA020',
-  otherReferencesForMovement: [
-    {
-      label: 'PO Number',
-      reference: 'PO-12345'
-    },
-    {
-      label: 'Waste Ticket',
-      reference: 'WT-67890'
-    },
-    {
-      label: 'Haulier Note',
-      reference: 'HN-11111'
-    }
-  ],
-  specialHandlingRequirements:
-    'The waste must be fully inspected by the waste handler according to the Hazardous waste consignment and or EWC codes provided.',
-  wasteItems: [
-    {
-      ewcCodes:
-        '200108 biodegradable kitchen and canteen waste, 150109 textile packaging',
-      wasteDescription:
-        'Basic mixed construction and demolition waste, this includes recyclable house bricks, gypsum plaster and slates.',
-      physicalForm: 'Sludge',
-      numberOfContainers: '2, 3',
-      typeOfContainers: 'BAG, PAL, SKI',
-      weight: {
-        metric: 'Tonnes',
-        amount: 150,
-        isEstimate: true
-      },
-      containsPops: true,
-      pops: {
-        sourceOfComponents: 'PROVIDED_WITH_WASTE',
-        components: [
-          {
-            code: 'PFHXS, HBB, END, PCDD_PCDF',
-            concentration: '12.5, 9,21234, 500'
-          }
-        ]
-      },
-      containsHazardous: true,
-      hazardous: {
-        sourceOfComponents: 'PROVIDED_WITH_WASTE',
-        hazCodes:
-          'HP 5 - Wastes from petroleum refining, Natural Gas Purification and pyrolitic treatment of coal, HP 10 - Wastes from Thermal Processes</p>',
-        components: [
-          {
-            name: 'lead, mercury',
-            concentration: '50, 12.5, 25.5'
-          }
-        ]
-      },
-      disposalOrRecoveryCodes: [
-        {
-          code: '"R1"\n',
-          weight: {
-            metric: 'Tonnes',
-            amount: 150,
-            isEstimate: true
-          }
-        }
-      ]
-    }
-  ],
-  carrier: {
-    registrationNumber:
-      'England - CBDL6, CBDU45, CBDU84916, Wales - CBDL1, CBDU33, CBDU84916, Scotland - WCR/522048, Northern Ireland - ROC UT 36, ROC LT 5432',
-    reasonForNoRegistrationNumber:
-      'Waste carrier did not provide a carrier registration number.',
-    organisationName: 'Waste Carriers Lite Ltd',
-    address: {
-      fullAddress: '26a Oil Drum Lane, London, UK',
-      postcode: 'W12 7ZL'
-    },
-    emailAddress: 'info@wastecarrierselite.co.uk',
-    phoneNumber: '020 4756 XXXX',
-    vehicleRegistration: 'RNT 493',
-    meansOfTransport: 'Rail'
-  },
-  brokerOrDealer: {
-    organisationName: 'Waste Desposal Ltd',
-    address: {
-      fullAddress: '26a Oil Drum Lane, London, UK',
-      postcode: 'W12 7ZL'
-    },
-    emailAddress: 'info@wastecarrierselite.co.uk',
-    phoneNumber: '020 4756 XXXX',
-    registrationNumber:
-      'CBDL6, CBDU45, CBDU84916, Wales - CBDL1, CBDU33, Scotland - WCR/R/522048, Northern Ireland - ROC UT 36, ROC LT 5432'
-  },
-  receiver: {
-    siteName: 'string',
-    emailAddress: 'info@wastecarrierselite.co.uk',
-    phoneNumber: '020 4756 XXXX',
-    authorisationNumber: 'HP3456XX',
-    regulatoryPositionStatements: [343, 456, 789]
-  },
-  receipt: {
-    address: {
-      fullAddress: '26a Oil Drum Lane, London, UK',
-      postcode: 'W12 7ZL'
-    }
-  }
-}
-*/
