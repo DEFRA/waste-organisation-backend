cd3a9b4950cc30f5630d912787afee324ec51626
Ed Bowler <ed.bowler@equalexperts.com>
2026-03-24 17:44:25 +0000

    DWT-1897: fix duplicate errors when composing transforms (#81)

--- src/services/spreadsheetImport/transforms.test.js
+++ src/services/spreadsheetImport/transforms.test.js
@@ -63,4 +63,28 @@ describe('compose', () => {
     )
     expect(f).toThrowError(expect.objectContaining({ collectedErrors: [e2, e3, e1] }))
   })
+
+  test('should reset collected exceptions on multiple calls', () => {
+    const e1 = new Error('1')
+    const e2 = new Error('2')
+    const f = compose(
+      (x) => {
+        console.log(`after - ${x}`)
+        return x
+      },
+      () => {
+        throw e1
+      },
+      () => {
+        throw e2
+      },
+      () => {
+        console.log(`before`)
+        return 'before'
+      }
+    )
+    expect(f).toThrowError(expect.objectContaining({ collectedErrors: [e2, e1] }))
+    expect(f).toThrowError(expect.objectContaining({ collectedErrors: [e2, e1] }))
+    expect(f).toThrowError(expect.objectContaining({ collectedErrors: [e2, e1] }))
+  })
 })
