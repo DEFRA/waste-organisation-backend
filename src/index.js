eeacdfda4c0d3dad15d32fa985e619c3b767644f
Ed Bowler <ed.bowler@equalexperts.com>
2026-04-15 17:00:35 +0100

    DWT-2257: add config for timezone - used in parsing spreadsheet date

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import process from 'node:process'
 import { createLogger } from './common/helpers/logging/logger.js'
 import { createServer, startServer } from './api-server.js'
 import { startWorker } from './backgroundProcessor.js'
+import { config } from './config.js'
+
+process.env.TZ = config.get('bulkUpload.spreadsheetTimezone')
 
 startWorker()
 
