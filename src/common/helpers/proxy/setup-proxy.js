c520df62d7759fa07d23ce95b4b834790d481b66
Ed Bowler <ed.bowler@equalexperts.com>
2026-06-22 16:21:01 +0100

    DWT-2442: remove proxy setup (#157)
    * DWT-2442: remove proxy setup
    
    * DWT-2442: remove client that depends on axios
    
    * DWT-2442: seems to work in dev, but mocking in tests is broken
    
    * DWT-2442: fixing tests
    
    DWT-2442: test a different prefix
    
    DWT-2442: try string concatenation
    
    * DWT-2442: fix test after changing return val
    
    * DWT-2442: add tests
    
    * DWT-2442: skip magic number warning for gov notify key parsing
    
    * DWT-2442: fixing test

--- src/common/helpers/proxy/setup-proxy.js
+++ src/common/helpers/proxy/setup-proxy.js
@@ -1,27 +1,16 @@
-import { ProxyAgent, setGlobalDispatcher } from 'undici'
-import { bootstrap } from 'global-agent'
-
+import http from 'node:http'
+import https from 'node:https'
+import Wreck from '@hapi/wreck'
 import { createLogger } from '../logging/logger.js'
 import { config } from '../../../config.js'
 
 const logger = createLogger()
 
-/**
- * If HTTP_PROXY is set setupProxy() will enable it globally
- * for a number of http clients.
- * Node Fetch will still need to pass a ProxyAgent in on each call.
- */
 export function setupProxy() {
-  const proxyUrl = config.get('httpProxy')
-
-  if (proxyUrl) {
-    logger.info('setting up global proxies')
-
-    // Undici proxy
-    setGlobalDispatcher(new ProxyAgent(proxyUrl))
-
-    // global-agent (axios/request/and others)
-    bootstrap()
-    global.GLOBAL_AGENT.HTTP_PROXY = proxyUrl
+  if (config.get('httpProxy')) {
+    logger.info('Routing outbound requests via proxy')
+    // Required for Wreck
+    Wreck.agents.http = http.globalAgent
+    Wreck.agents.https = https.globalAgent
   }
 }
