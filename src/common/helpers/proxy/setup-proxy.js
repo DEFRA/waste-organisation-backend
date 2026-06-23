88bbb90c357cc207475c3d70920148d1f1279593
Ed Bowler <ed.bowler@equalexperts.com>
2026-06-23 15:59:02 +0100

    DWT-2442: fix import

--- src/common/helpers/proxy/setup-proxy.js
+++ src/common/helpers/proxy/setup-proxy.js
@@ -1,9 +1,8 @@
-import http from 'node:http'
-import https from 'node:https'
 import Wreck from '@hapi/wreck'
 import { createLogger } from '../logging/logger.js'
 import { config } from '../../../config.js'
 import Https from 'node:https'
+import Http from 'node:http'
 
 const logger = createLogger()
 
@@ -11,8 +10,8 @@ export const setupProxy = () => {
   if (config.get('httpProxy')) {
     logger.info('Routing outbound requests via proxy')
     // Required for Wreck
-    Wreck.agents.http = http.globalAgent
-    Wreck.agents.https = https.globalAgent
+    Wreck.agents.http = Http.globalAgent
+    Wreck.agents.https = Https.globalAgent
   }
   return Wreck
 }
