202ca5f297f7e837eb5b54292b7d5ff601b7a262
Ed Bowler <ed.bowler@equalexperts.com>
2026-06-30 14:32:57 +0100

    DR-54: refund polling - wip

--- src/config.js
+++ src/config.js
@@ -176,6 +176,13 @@ export const config = convict({
       nullable: false,
       default: 60 * 60 * 1000, // nosonar
       env: 'GOVPAY_SERVICE_CHARGE_PENDING_CREATE_PAYMENT_TIMEOUT'
+    },
+    refundPollingSchedule: {
+      doc: 'The refund polling cron expression',
+      format: String,
+      nullable: true,
+      default: '30 3 * * *',
+      env: 'GOVPAY_REFUND_POLLING_SCHEDULE'
     }
   },
   aws: {
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import process from 'node:process'
 import { createLogger } from './common/helpers/logging/logger.js'
 import { createServer, startServer } from './api-server.js'
 import { startWorker } from './backgroundProcessor.js'
+import { startTasks } from './scheduledJobs.js'
 import { config } from './config.js'
 
 process.env.TZ = config.get('bulkUpload.spreadsheetTimezone')
 
 startWorker()
+
+startTasks()
 
 await startServer(await createServer())
 
