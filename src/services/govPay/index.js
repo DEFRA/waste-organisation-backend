0867034db1b70f9db59c88364b0a369a8a150129
Ed Bowler <ed.bowler@equalexperts.com>
2026-07-02 15:43:58 +0100

    DR-54: added service api

--- src/services/govPay/index.js
+++ src/services/govPay/index.js
@@ -3,6 +3,7 @@ import { CREATED, SUCCESS } from '../httpStatusCodes.js'
 import { createLogger } from '../../common/helpers/logging/logger.js'
 import wreck from '@hapi/wreck'
 import { createAgent } from '../../common/helpers/proxy/setup-proxy.js'
+import { setTimeout } from 'timers/promises'
 
 const fallbackLogger = createLogger()
 
@@ -52,5 +53,56 @@ export const getPaymentStatus = async (paymentId, logger) => {
   } catch (e) {
     log.error(`Error initiating payment ${e} ${e.stack}`)
     return { status: 'error', error: e }
+  }
+}
+
+const formatDate = (d) => d.toISOString().replace(/.[0-9][0-9][0-9]Z$/, 'Z')
+
+/* 
+  from: https://docs.payments.service.gov.uk/api_reference/#pagination
+Pagination links
+Search endpoints also return a _links object, which includes href and method fields you can use to move between pages. Use the fields in:
+
+self to run the same search again
+first_page to get the first page of results
+last_page to get the last page
+prev_page to get the previous page
+next_page to get the next page
+ */
+export async function* getRefundsBetween(start, end, logger) {
+  const log = logger ?? fallbackLogger
+  const maxRetries = 5
+  logger.debug(`fetching refund data between ${start} and ${end}`)
+  const { apiUrl, apiKey } = config.get('govPay')
+  let nextUrl = `${apiUrl.replace(/\/$/, '')}/refunds?from_date=${formatDate(start)}&to_date=${formatDate(end)}&display_size=10`
+  let i = 0
+  while (nextUrl) {
+    try {
+      const { res, payload } = await wreck.get(nextUrl, {
+        json: true,
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          'Content-Type': 'application/json'
+        },
+        agent
+      })
+      logger.debug(`fetched ${res?.statusCode}`)
+      if (res?.statusCode === SUCCESS) {
+        i = maxRetries + 1
+        nextUrl = payload?._links?.next_page?.href
+        logger.debug(` >> ${payload.results}`)
+        yield* payload.results
+      } else {
+        throw new Error('error status code')
+      }
+    } catch (e) {
+      log.error(`Error initiating payment ${e}  >> retry ${i} ${e.stack}`)
+      i++
+      if (i > 100) {
+        throw e
+      } else {
+        await setTimeout(i * config.get('govPay.schedulingPollingTaskRetrySleepStep'))
+      }
+    }
   }
 }
---
+++ src/services/govPay/index.test.js
@@ -0,0 +1,45 @@
+import { beforeAll, describe, expect, vi } from 'vitest'
+import { config } from '../../config.js'
+
+describe('govpay', () => {
+  const wreckGetMock = vi.fn()
+
+  beforeAll(async () => {
+    config.set('govPay.schedulingPollingTaskRetrySleepStep', 1)
+    vi.doMock('@hapi/wreck', () => ({
+      default: {
+        get: wreckGetMock
+      }
+    }))
+  })
+  afterAll(() => {
+    vi.clearAllMocks()
+  })
+
+  test('generator func paginates correctly', async () => {
+    let govPayResponses = [
+      { res: { statusCode: 200 }, payload: { results: [1, 2, 3], _links: { next_page: { href: 'test' } } } },
+      { res: { statusCode: 'timeout' } },
+      { res: { statusCode: 200 }, payload: { results: [4, 5, 6], _links: { next_page: { href: 'test' } } } },
+      { res: { statusCode: 'timeout' } },
+      { res: { statusCode: 'timeout' } },
+      { res: { statusCode: 'timeout' } },
+      { res: { statusCode: 'timeout' } },
+      { res: { statusCode: 'timeout' } },
+      { res: { statusCode: 'timeout' } },
+      { res: { statusCode: 200 }, payload: { results: [7, 8, 9], _links: { next_page: { href: 'test' } } } },
+      { res: { statusCode: 200 }, payload: { results: [10], _links: { next_page: {} } } }
+    ]
+    let page = 0
+    wreckGetMock.mockImplementation(async () => {
+      return govPayResponses[page++]
+    })
+    const { getRefundsBetween } = await import('./index.js')
+    let i = 0
+    for await (const x of getRefundsBetween(new Date(), new Date(), console)) {
+      i++
+      expect(x).toBe(i)
+    }
+    expect(i).toBe(10)
+  })
+})
