64f3b8180667535a0189b688831c4e38b1f355e8
Philip J Smith <philip.j.smith@contractsquared.com>
2026-07-14 15:55:14 +0100

    added aws-sdk credential-providers

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import { config } from './config.js'
 process.env.TZ = config.get('bulkUpload.spreadsheetTimezone')
 
 await startServer(await createServer())
-
 startWorker()
-
 const { stopScheduling } = await startTasks()
 
 process.on('unhandledRejection', (error) => {
