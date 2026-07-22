92dd5274937c6e06495d4f61bf69475395cec3f9
Ed Bowler <ed.bowler@equalexperts.com>
2026-07-22 16:33:10 +0100

    DR-54: Restructure lock manager use to fix concurrent runs (#206)
    * DR-54: Restructure lock manager use to fix concurrent runs
    
    * DR-54: use easier api in scheduled jobs
    
    * DR-54: added test
    
    * DR-54: npm audit
    
    * DR-54: formatting
    
    * DR-54: sonarcloud changes
    
    * DR-54: add test
    
    * DR-54: upgrade dep
    
    * DR-54: remove hack to get around buggy version of mongo-locks
    
    * DR-54: fix flakey test
    
    * DR-54: ignore false positive

--- src/plugins/mongo-lock.js
+++ src/plugins/mongo-lock.js
@@ -1,4 +1,6 @@
-async function acquireLock(locker, resource, logger) {
+import { LockManager } from 'mongo-locks'
+
+export const acquireLock = async (locker, resource, logger) => {
   const lock = await locker.lock(resource)
   if (!lock) {
     if (logger) {
@@ -9,7 +11,7 @@ async function acquireLock(locker, resource, logger) {
   return lock
 }
 
-async function requireLock(locker, resource) {
+export const requireLock = async (locker, resource) => {
   const lock = await locker.lock(resource)
   if (!lock) {
     throw new Error(`Failed to acquire lock for ${resource}`)
@@ -17,4 +19,30 @@ async function requireLock(locker, resource) {
   return lock
 }
 
-export { acquireLock, requireLock }
+export const createLockManager = async (db) => new LockManager(db.collection('mongo-locks'))
+
+export const singletonRunner = (() => {
+  const lockManagers = new WeakMap()
+  return async (db, label, logger, func, noLockFunc) => {
+    logger.debug(`db ${db.databaseName}`)
+    if (lockManagers.get(db) == null) {
+      lockManagers.set(db, await createLockManager(db))
+    }
+    const lm = lockManagers.get(db)
+    logger.debug(`Acquiring lock for ${label} with lock manager ${lm}`)
+    const lock = await acquireLock(lm, label, logger)
+    if (!lock) {
+      if (typeof noLockFunc === 'function') {
+        noLockFunc(label, logger)
+      }
+      return null
+    }
+    logger.debug(`Lock for ${label} acquired - running func`)
+    try {
+      return await func()
+    } finally {
+      logger.debug(`freeing lock for ${label}`)
+      await lock.free()
+    }
+  }
+})()
--- src/plugins/mongodb.test.js
+++ src/plugins/mongodb.test.js
@@ -1,21 +1,18 @@
-import { Db, MongoClient, ObjectId } from 'mongodb'
+import { Db, MongoClient } from 'mongodb'
 import { LockManager } from 'mongo-locks'
 import { initialiseServer, stopServer } from '../common/helpers/initialse-test-server.js'
-import { setMissingCreatedAtAndUpdatedAtOrganisationFields } from './mongodb.js'
-import { mock } from 'node:test'
 
 describe('#mongoDb', () => {
-  let server
-
-  beforeAll(async () => {
-    server = await initialiseServer()
-  })
-
-  afterAll(async () => {
-    stopServer(server)
-  })
-
   describe('Set up', () => {
+    let server
+
+    beforeAll(async () => {
+      server = await initialiseServer()
+    })
+
+    afterAll(async () => {
+      stopServer(server)
+    })
     test('Server should have expected MongoDb decorators', () => {
       expect(server.db).toBeInstanceOf(Db)
       expect(server.mongoClient).toBeInstanceOf(MongoClient)
@@ -33,73 +30,11 @@ describe('#mongoDb', () => {
 
   describe('Shut down', () => {
     test('Should close Mongo client on server stop', async () => {
+      const server = await initialiseServer()
       const closeSpy = vi.spyOn(server.mongoClient, 'close')
       await server.stop({ timeout: 1000 })
 
       expect(closeSpy).toHaveBeenCalledWith(true)
     })
-  })
-})
-
-describe('#setMissingCreatedAtAndUpdatedAtOrganisationFields', () => {
-  const mockUpdateOne = vi.fn()
-  const mockDb = (organisations) => ({
-    collection: () => ({
-      find: () => ({
-        toArray: () => organisations
-      }),
-      updateOne: mockUpdateOne
-    })
-  })
-  const mockLogger = {
-    error: vi.fn()
-  }
-
-  test('Should update Organisations when Organisations are found', async () => {
-    const organisation1Id = new ObjectId('69a6c7e93fa2e7991f78c61d')
-    const organisation1Date = new Date('2026-03-03T11:37:13.000Z')
-    const organisation2Id = new ObjectId('69a6c7f03fa2e7991f78ca35')
-    const organisation2Date = new Date('2026-03-03T11:37:20.000Z')
-    const organisations = [{ _id: organisation1Id }, { _id: organisation2Id }]
-
-    await setMissingCreatedAtAndUpdatedAtOrganisationFields(mockDb(organisations), mockLogger)
-
-    expect(mockUpdateOne)
-      .toHaveBeenCalledTimes(2)
-      .toHaveBeenCalledWith({ _id: organisation1Id }, { $set: { createdAt: organisation1Date, updatedAt: organisation1Date } })
-      .toHaveBeenCalledWith({ _id: organisation2Id }, { $set: { createdAt: organisation2Date, updatedAt: organisation2Date } })
-  })
-
-  test('Should not update Organisations when no Organisations are found', async () => {
-    const organisations = []
-
-    await setMissingCreatedAtAndUpdatedAtOrganisationFields(mockDb(organisations), mockLogger)
-
-    expect(mockUpdateOne).not.toHaveBeenCalled()
-  })
-
-  test('Should default to current date if date cannot be derived from Organisation id', async () => {
-    const organisations = [{ _id: 'id123' }]
-    const date = new Date('2026-07-08')
-
-    mock.timers.enable({ apis: ['Date'], now: date })
-
-    await setMissingCreatedAtAndUpdatedAtOrganisationFields(mockDb(organisations), mockLogger)
-
-    expect(mockUpdateOne)
-      .toHaveBeenCalledTimes(1)
-      .toHaveBeenCalledWith({ _id: 'id123' }, { $set: { createdAt: date, updatedAt: date } })
-  })
-
-  test('Should log and not throw errors', async () => {
-    const error = 'Internal Server Error'
-    const mockDb = {
-      collection: () => {
-        throw new Error(error)
-      }
-    }
-
-    await expect(() => setMissingCreatedAtAndUpdatedAtOrganisationFields(mockDb, mockLogger)).not.toThrow()
-    expect(mockLogger.error).toHaveBeenCalledWith(`Failed to set missing createdAt and updatedAt Organisation fields: ${error}`)
   })
 })
